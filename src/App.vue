dc9e4429bc72e0901cd247412329091419362a9c
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2021-12-03 10:10:06 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let count = ref(0);
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center h-screen text-gray-900">
+    <h1 class="font-bold text-6xl">Count {{count}}</h1>
+    <button class="border border-gray-900 p-3 rounded-full w-52 mt-5" @click="count++">Increment</button>
+    <button class="border border-gray-900 p-3 rounded-full w-52 mt-5" @click="count--">Decrement</button>
+  </div>
+</template>
+
+<style lang="scss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+@tailwind variants;
+</style>
